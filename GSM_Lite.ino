4a13ad169d8db909601f82c3ad4d54d35dbf0828
NIkolay Dementev <born.ru53@gmail.com>
2017-02-04 12:53:33 +0300

    Add files via upload

---
+++ GSM_Lite.ino
@@ -0,0 +1,297 @@
+#include <EEPROM.h>
+#include <OneWire.h>
+#define zone1 A0
+#define Zone1LED 4
+#define alarm1LED 5
+#define power A3
+int statusPower;
+byte powerBad;
+int statusZ;
+int timeOUT;
+int timeIN;
+int statusO;
+int powerTime;
+byte alarm;
+byte armed = 0;
+byte armedIN = 0;
+byte balarm = 0;
+const byte saveKey = 7; // вход для кнопки обнуления
+const byte statusLed = 6;
+OneWire  ds(8);
+
+byte addr[8];
+byte allKey; // всего ключей
+
+// функция сверяет два адреса (два массива)
+boolean addrTest(byte addr1[8], byte addr2[8]){
+  for(int i = 0; i < 8; i++) {
+    if (addr1[i] != addr2[i]) return 0;
+  }
+  return 1;
+}
+
+void error(){
+  while(1){
+    digitalWrite(statusLed, !digitalRead(statusLed));
+    delay(300);
+    digitalWrite(statusLed, !digitalRead(statusLed));
+    delay(300);
+    digitalWrite(statusLed, !digitalRead(statusLed));
+    delay(300);
+    break;
+
+  }
+}//
+
+boolean keyTest(){ // возвращает 1 если ключ есть в еепром
+  byte addrTemp[8];  
+  for (int i = 0; i < allKey; i++){  
+    for(int y = 0; y < 8; y++) addrTemp[y] = EEPROM.read((i << 3) + y );   
+    if (addrTest(addrTemp, addr)) return 1;
+
+  }     
+  return 0;
+}//
+
+void save(){ // сохраняет ключ в еепром
+
+  digitalWrite(statusLed, HIGH);    
+  if (allKey >= 10) error(); // если места нет 
+
+  while (!ds.search(addr)) ds.reset_search(); // ждем ключ 
+  if ( OneWire::crc8( addr, 7) != addr[7]) error();
+  if (keyTest()) error(); // если нашли ключ в еепром.
+
+  for(int i = 0; i < 8; i++) EEPROM.write((allKey << 3) + i, addr[i]);    
+  delay(3);
+  allKey++; // прибавляем единицу к количеству ключей 
+  EEPROM.write(511, allKey); 
+  digitalWrite(statusLed, LOW);
+  delay(300);
+}
+
+
+
+void setup () {
+  Serial.begin(9600);
+  pinMode(zone1, INPUT);
+  pinMode(Zone1LED, OUTPUT);
+  pinMode(alarm1LED, OUTPUT);
+  pinMode(statusLed, OUTPUT);  
+  pinMode(saveKey, INPUT_PULLUP);
+  pinMode(power, INPUT_PULLUP);
+
+
+  // если при включении нажата кнопка, сбрасываем ключи на 0 
+  if (!digitalRead(saveKey)) EEPROM.write(511, 0);
+
+  allKey = EEPROM.read(511); // читаем количество ключей
+}
+void loop () {
+  ds.reset_search();
+  Power ();
+  statusZ = analogRead(zone1);
+
+
+  if (keyTest()) 
+  {
+    button(); // если нашли ключ в еепром, 
+  }
+
+  if (armed == 0) 
+  {
+    digitalWrite (Zone1LED, LOW);
+    digitalWrite (alarm1LED, LOW);
+    alarm = 0;
+    timeOUT = 0;
+    timeIN = 0;
+    armedIN = 0;
+    balarm = 0;
+  }
+
+  if (armed == 1 && alarm == 0 && armedIN == 0)
+  {
+
+
+    if (timeIN == 200)// 100 - 10сек. 200 - 30сек. 300 - 1мин.
+    {
+      if (statusZ > 150 && statusZ < 200)
+      {
+        if (powerBad == 0)
+        {
+          Serial.println("Norma");
+          digitalWrite (Zone1LED, HIGH);
+          digitalWrite (alarm1LED, LOW);
+          armedIN = 1;
+        }
+        else 
+        {
+          digitalWrite (Zone1LED, HIGH);
+          delay(2000);
+          digitalWrite (alarm1LED, LOW);
+          digitalWrite (Zone1LED, LOW);
+          armedIN = 1;
+        }
+      }
+      else armed = 0;
+    }
+    if (armedIN ==0)
+    {
+      timeIN++;
+      if (powerBad == 0)
+      {
+        Serial.print("timeIN = ");
+        Serial.println(timeIN);
+        delay(timeIN);
+        digitalWrite (Zone1LED, HIGH);
+        delay(10);
+        digitalWrite (Zone1LED, LOW);
+        delay(10);
+        digitalWrite (Zone1LED, HIGH);
+        delay(10);
+        digitalWrite (Zone1LED, LOW);
+      }
+      else
+      {
+        delay(timeIN);
+        digitalWrite (Zone1LED, HIGH);
+        delay(3);
+        digitalWrite (Zone1LED, LOW);
+        delay(3);
+        digitalWrite (Zone1LED, HIGH);
+        delay(3);
+        digitalWrite (Zone1LED, LOW);
+      }
+    }
+
+
+  }
+  if (armed == 1 && alarm == 0)
+  {
+    if (balarm == 0 && armedIN == 1)
+    {
+      if (statusZ < 150 || statusZ > 200 )
+      {
+        balarm = 1;
+      }
+    }
+    if (timeOUT == 300)
+    {
+      if (powerBad == 0)
+      {
+        Serial.println("Alarm");
+        digitalWrite (Zone1LED, LOW);
+        digitalWrite (alarm1LED, HIGH);
+        //digitalWrite (STOP1LED, LOW);
+        alarm = 1;
+        timeOUT = 0;
+        balarm = 0;
+      }
+      else
+      {
+        digitalWrite (Zone1LED, LOW);
+        digitalWrite (alarm1LED, HIGH);
+        delay(2000);
+        digitalWrite (alarm1LED, LOW);
+        //digitalWrite (STOP1LED, LOW);
+        alarm = 1;
+        timeOUT = 0;
+        balarm = 0;
+      }
+    }
+    if (balarm == 1 && armedIN == 1)
+    {
+      timeOUT++;
+      if (powerBad == 0)
+      {
+        Serial.print("timeOUT = ");
+        Serial.println(timeOUT);
+        delay(timeOUT);
+        digitalWrite (alarm1LED, HIGH);
+        delay(10);
+        digitalWrite (alarm1LED, LOW);
+        delay(10);
+        digitalWrite (alarm1LED, HIGH);
+        delay(10);
+        digitalWrite (alarm1LED, LOW);
+      }
+      else
+      {
+        delay(timeOUT);
+        digitalWrite (alarm1LED, HIGH);
+        delay(3);
+        digitalWrite (alarm1LED, LOW);
+        delay(3);
+        digitalWrite (alarm1LED, HIGH);
+        delay(3);
+        digitalWrite (alarm1LED, LOW);
+      }
+    }
+  }
+
+  if (!digitalRead(saveKey) && armed == 0) save(); // если нажали кнопку  
+  // сканируем шину, если нет устройств выходим из loop
+  if (!ds.search(addr)) return; 
+  if ( OneWire::crc8( addr, 7) != addr[7]) return;
+}
+void button ()
+{
+  armed = !armed;
+  for(int i = 0; i < 8; i++) {
+    addr[i] = 0;
+  }
+
+  delay(2000);
+}
+void Power ()
+{
+  statusPower = analogRead(power);
+  if (statusPower >= 1010)
+  {
+    //Serial.println ("Power OK ");
+    powerBad = 0;
+    if (armed == 1 && alarm == 0 && digitalRead(Zone1LED) == LOW)
+    {
+      digitalWrite (Zone1LED, HIGH);
+    }
+    if (armed == 1 && alarm == 1 && digitalRead(alarm1LED) == LOW)
+    {
+      digitalWrite (alarm1LED, HIGH);
+    }
+  }
+  else 
+  {
+    // Serial.println ("Power BAD!!! ");
+    powerBad = 1;
+    if (powerTime == 10000)// 10000 - 17 сек. ????
+    {
+      digitalWrite(alarm1LED, HIGH);
+      delay(3);
+      digitalWrite(alarm1LED, LOW);
+      powerTime = 0;
+    }
+    powerTime ++;
+    if (armed == 1 && digitalRead(Zone1LED) == HIGH)
+    {
+      digitalWrite (Zone1LED, LOW);
+    }
+    if (armed == 1 && alarm == 1 && digitalRead(alarm1LED) == HIGH)
+    {
+      digitalWrite (alarm1LED, LOW);
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ GSM__SMS.ino
@@ -0,0 +1,155 @@
+/// GSM сигналка c установкой по звонку
+/// датчиком на прерывателях
+
+// 
+
+#include <EEPROM.h>
+
+//// как подключен модем?
+#include <SoftwareSerial.h>                 // если программный
+SoftwareSerial gsm(7, 8); // RX, TX
+//#define gsm Serial                           // если аппаратный в UNO
+//#define gsm Serial1                          // если аппаратный в леонардо
+
+#define LED 13
+
+#define TELLNUMBER "ATD+7;"                 // номен на который будем звонить
+#define SMSNUMBER "AT+CMGS=\"7\""           // номер на который будем отправлять SMS
+#define TELMODE "7"                         // номер для установки на охрану
+
+#define SH1 A2              // шлейыф
+//#define SH2 A3
+#define pinPOWER 4           //Наличие основного питания
+#define pinBATTERY A4        //Контроль напряжения резервного питания (аккамуляторов)
+#define pinBOOT 5           // нога BOOT или K на модеме 
+byte sms = 0;               //
+byte mode = 0;              // 0 - только включили
+// 1 - установлена охрана
+// 2 - снята с охраны
+// при добавлении не забываем посмотреть на 41 строку
+
+
+
+void setup() {
+  delay(1000);                    //// !! чтобы нечего не повисало при включении
+
+  gsm.begin(9600);                         /// незабываем указать скорость работы UART модема
+  //  Serial.begin(9600);
+  pinMode(pinPOWER,INPUT);
+  pinMode(pinBATTERY, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+  pinMode(pinBOOT, OUTPUT);                /// нога BOOT на модеме
+  pinMode(SH1, INPUT_PULLUP);              /// нога на растяжку
+  //  pinMode(SH2, INPUT_PULLUP);              /// нога на растяжку
+  gsm.println("AT+CMGF=1");  
+  delay(100);
+  gsm.println("AT+CSCS=\"GSM\"");   
+  delay(100);
+  // читаем режим из еепром
+  mode = EEPROM.read(0);
+  if (mode > 2) mode = 2;                 // проверяем значение в еепром 
+  // занимаемся модемом        
+  delay(1000);                            
+  digitalWrite(LED, HIGH);                // на время включаем лед  
+  digitalWrite(pinBOOT, LOW);             /// включаем модем 
+
+  // нужно дождатся включения модема и соединения с сетью
+  delay(2000);    
+  //  while(gsm.find("STARTUP"));          /// ждем команды от модема  
+  gsm.println("ATE0");                  // выключаем эхо  
+
+  while(1){                             // ждем подключение модема к сети
+    gsm.println("AT+COPS?");
+    if (gsm.find("+COPS: 0")) break;
+    digitalWrite(LED, LOW);               // блымаем светодиодом
+    delay(50);  
+    digitalWrite(LED, HIGH);  
+    delay(500); 
+  }
+
+  //Serial.println("Modem OK"); 
+  digitalWrite(LED, LOW);               // блымаем светодиодом
+  delay(1500);  
+  digitalWrite(LED, HIGH);
+  delay(250); 
+  digitalWrite(LED, LOW);   
+
+}
+
+void loop() {
+  if(digitalRead(pinPOWER)== LOW){     // если нажали кнопку
+    if (sms == 2){
+      while(1){             // проверяем готовность модема
+        gsm.println("AT+CPAS");
+        if (gsm.find("0")) break;
+        delay(100);  
+      }
+      gsm.println (SMSNUMBER); // даем команду на отправку смс
+      delay(100);
+      gsm.print("POWER Down");  // отправляем текст
+      //gsm.print(analogRead(A0));    // и переменную со значением
+      gsm.print((char)26);          // символ завершающий передачу
+      // Serial.println("ok");
+      if (gsm.find("OK")){
+        sms++;
+      }
+    }
+  }
+
+  if (mode == 1){                         // если в режиме охраны
+    // проверяем датчики
+    if (digitalRead(SH1)){                // если обрыв
+      // отзваниваемся
+      gsm.println(TELLNUMBER); 
+      delay(2500);                       
+      if (gsm.find("NO CARRIER")){      // ищим сброс вызова,  
+        // снимаем охранку
+        mode = 2;
+        EEPROM.write(0, mode);
+      }
+    }
+  }
+
+
+  // если охрана снята
+  if (mode == 2){
+    if (digitalRead(SH1)){  // проверяем датчики, включаем LED
+      digitalWrite(LED, HIGH);    
+    }
+    else digitalWrite(LED, LOW);
+  }
+
+
+  // ищим RING
+  // если нашли, опрашиваем кто это и ставим на охрану
+  if(gsm.find("RING")){                    // если нашли RING
+    gsm.println("AT+CLIP=1");              // включаем АОН, 
+
+    while(1){                              // в цикле
+      if (gsm.find(TELMODE)){               // ищим номер телефона, если нашли
+        mode = 1;                           // меняем режим  
+        EEPROM.write(0, mode);              // пишим его в еепром
+        break;                              // и выходим
+      }  
+      else{                                 // иначе 
+        gsm.println("AT+CPAS");             // спрашиваем состояние модема   
+        delay(100);
+        if (gsm.find("+CPAS: 0")) break;    // и если он в "готовности", выходим из цикла
+      }                                     // если звонок в процессе, возвращает +CPAS: 3
+    }                                      // и крутимся дальше
+
+    gsm.println("AT+CLIP=0");            // выключаем АОН, 
+    delay(500);
+    gsm.println("ATH0");                 // сбрасываем вызов 
+
+    digitalWrite(LED, LOW);             // сигнализируем об этом
+    delay(500);  
+    digitalWrite(LED, HIGH);
+    delay(250); 
+    digitalWrite(LED, LOW);   
+
+  } 
+
+}
+
+
